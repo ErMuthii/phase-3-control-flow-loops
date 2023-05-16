2a0565d3fc1c3b8536186f106fce306f30fb016b
Mary Wanjiku <er.muthii@gmail.com>
2023-05-16 13:06:47 +0300

    Fixed

--- looping.rb
+++ looping.rb
@@ -21,18 +21,10 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  (1..100).each do |num|
-    
-    if num % 3 == 0 && num % 5 == 0
-      "FizzBuzz"
-    elsif num % 3 == 0
-      "Fizz"
-    elsif num % 5 == 0
-      "Buzz"
-    else
-      num
-    end
 
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+    end
 end
 
 def reverse_string(str)
